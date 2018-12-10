5affb402c718ba07d6079f370072836de3a4d8a9
Lysenko Artem <34771157+virrius@users.noreply.github.com>
2018-12-10 21:25:29 +0300

    test lambda

--- lambdas/authFunction/index.js
+++ lambdas/authFunction/index.js
@@ -2,10 +2,8 @@ console.log('PostgreSQL GET Function');
 var pg = require("pg");
 exports.handler = function(event, context,callback) {
     console.log('Received event : ' + JSON.stringify(event) + ' at ' + new Date());
-    let userData=JSON.parse(event.body);
-    let name= userData["name"];
-    let mail = userData["email"];
-    let password = userData["password"];
+    console.log(event.body['agility']);
+    console.log(event.body.split('&'));
     console.log(name,mail,password);
 
 
