5fb872fd5df9c7600bbe485ff0e8c618d43dc330
Lysenko Artem <34771157+virrius@users.noreply.github.com>
2018-12-11 02:09:02 +0300

    saveformLambda

--- lambdas/saveFormFunction/index.js
+++ lambdas/saveFormFunction/index.js
@@ -10,6 +10,8 @@ exports.handler = function(event, context,callback) {
         obj[data[key].split("=")[0]] = data[key].split("=")[1];
     }
 
+    console.log(req.body);
+
     let conn = ({
         host: 'characterdb.c4mkdpklb4rf.us-east-2.rds.amazonaws.com',
         port: 5432,
@@ -27,33 +29,105 @@ exports.handler = function(event, context,callback) {
                     statusCode: '200',
                     "headers": {
                         "Access-Control-Allow-Origin": "*",
-                        'Access-Control-Allow-Methods': 'POST'
+                        'Access-Control -Allow-Methods': 'POST'
                     },
                     body: 'Ошибка соединения с базой данных '
                 });
             });
         }
         else {
+            var characteristicsID;
+            var skillsID;
+            let userName=obj['userName'];
             console.log('Connection established with pg db server');
-            let SQL1="SELECT column_name FROM information_schema.columns WHERE table_name ='characteristics';";
+            let SQL1 = "SELECT column_name FROM information_schema.columns WHERE table_name ='characteristics';";
             console.log(SQL1);
             client.query("SELECT column_name FROM information_schema.columns WHERE table_name ='characteristics';",
-                (err, characteristics)=> {
-                    client.end(function (err) {
-                        callback(null, {
-                            statusCode: '200',
-                            "headers": {
-                                "Access-Control-Allow-Origin": "*",
-                                'Access-Control-Allow-Methods': 'POST'
-                            },
-                            body: characteristics.rows
-                        });
-                    })
-                })
-            }
+                (err, characteristics) => {
+                    let SQLcolumns="";
+                    let SQLvalues="";
+                    characteristics.rows.forEach(function (item, i, arr) {
+                        let column=item['column_name'];
+                        if(obj[column]!==undefined)
+                        {
+                            SQLcolumns += column + ", ";
+                            SQLvalues+="'"+obj[column]+"', ";
+                        }
+                    });
+                    SQLcolumns=SQLcolumns.slice(0,-2);
+                    SQLvalues=SQLvalues.slice(0,-2);
+                    console.log(SQLcolumns, SQLvalues);
+                    let SQL2="INSERT INTO characteristics("+SQLcolumns+") VALUES("+SQLvalues+") RETURNING id;";
+                    console.log(SQL2);
+                    client.query("INSERT INTO characteristics("+SQLcolumns+") VALUES("+SQLvalues+") RETURNING id;",
+                        (err, id)=>
+                        {
+                            characteristicsID=id.rows[0]['id'];
+                            console.log(characteristicsID);
+                            let SQL11 = "SELECT column_name FROM information_schema.columns WHERE table_name ='skills';";
+                            console.log(SQL11);
+                            client.query("SELECT column_name FROM information_schema.columns WHERE table_name ='skills';",
+                                (err, skills) => {
+                                    let SQLcolumns="";
+                                    let SQLvalues="";
+                                    skills.rows.forEach(function (item, i, arr) {
+                                        let column=item['column_name'];
+                                        if(obj[column]!==undefined)
+                                        {
+                                            SQLcolumns += column + ", ";
+                                            SQLvalues+="'"+obj[column]+"', ";
+                                        }
+                                    });
+                                    SQLcolumns=SQLcolumns.slice(0,-2);
+                                    SQLvalues=SQLvalues.slice(0,-2);
+                                    console.log(SQLcolumns, SQLvalues);
+                                    let SQL22="INSERT INTO skills("+SQLcolumns+") VALUES("+SQLvalues+") RETURNING id;";
+                                    console.log(SQL22);
+                                    client.query("INSERT INTO skills("+SQLcolumns+") VALUES("+SQLvalues+") RETURNING id;",
+                                        (err, id)=>
+                                        {
+                                            skillsID=id.rows[0]['id'];
+                                            console.log(skillsID, characteristicsID);
+                                            let SQL111="SELECT column_name FROM information_schema.columns WHERE table_name ='characters';"
+                                            console.log(SQL111);
+                                            client.query("SELECT column_name FROM information_schema.columns WHERE table_name ='characters';",
+                                                (err, attributes) => {
+                                                    let SQLcolumns="";
+                                                    let SQLvalues="";
+                                                    attributes.rows.forEach(function (item, i, arr) {
+                                                        let column=item['column_name'];
+                                                        if(obj[column]!==undefined)
+                                                        {
+                                                            SQLcolumns += column + ", ";
+                                                            SQLvalues+="'"+obj[column]+"', ";
+                                                        }
+                                                    });
+                                                    SQLcolumns+="owner, "+"characteristics, "+"skills";
+                                                    SQLvalues+="'"+userName+"', '"+characteristicsID.toString()+"', '"+skillsID.toString()+"'";
+                                                    console.log(SQLcolumns, SQLvalues);
+                                                    let SQL22="INSERT INTO characters("+SQLcolumns+") VALUES("+SQLvalues+")";
+                                                    console.log(SQL22);
+                                                    client.query("INSERT INTO characters("+SQLcolumns+") VALUES("+SQLvalues+") RETURNING id;",
+                                                        (err,res)=>
+                                                        {
+
+                                                            console.log(res+" success");
+                                                        }
+                                                    )
+
+                                                })
+                                        }
+                                    )
+
+                                });
 
 
+                        }
+                    )
 
+                });
+
+        }
 
 
     });
