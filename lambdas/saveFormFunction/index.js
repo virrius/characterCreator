d6c7bc147dcf0efd432fe7c5210d5757b8489023
Lysenko Artem <34771157+virrius@users.noreply.github.com>
2018-12-10 22:50:28 +0300

    Update index.js

--- lambdas/saveFormFunction/index.js
+++ lambdas/saveFormFunction/index.js
@@ -2,77 +2,59 @@ console.log('PostgreSQL GET Function');
 var pg = require("pg");
 exports.handler = function(event, context,callback) {
     console.log('Received event : ' + JSON.stringify(event) + ' at ' + new Date());
-    console.log(event.body['agility']);
-    console.log(event.body.split('&'));
-/*
+    let data = event.data.split("&");
+    var obj={};
+    for(let key in data)
+    {
+        console.log(data[key]);
+        obj[data[key].split("=")[0]] = data[key].split("=")[1];
+    }
 
-        let conn = ({
-            host: 'characterdb.c4mkdpklb4rf.us-east-2.rds.amazonaws.com',
-            port: 5432,
-            user: 'virrius',
-            password: 'yaalz2027',
-            database: 'CharacterDB'
-        });
-        let client = new pg.Client(conn);
-        let err=false;
-        client.connect((err) => {
-            if (err) {
-                console.error('Error connecting to pg server' + err.stack);
-                client.end(function (err) {
+    let conn = ({
+        host: 'characterdb.c4mkdpklb4rf.us-east-2.rds.amazonaws.com',
+        port: 5432,
+        user: 'virrius',
+        password: 'yaalz2027',
+        database: 'CharacterDB'
+    });
+
+    let client = new pg.Client(conn);
+    client.connect((err) => {
+        if (err) {
+            console.error('Error connecting to pg server' + err.stack);
+            client.end(function (err) {
+                callback(null, {
+                    statusCode: '200',
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        'Access-Control-Allow-Methods': 'POST'
+                    },
+                    body: 'Ошибка соединения с базой данных '
+                });
+            });
+        }
+        else {
+            console.log('Connection established with pg db server');
+            let SQL1="SELECT column_name FROM information_schema.columns WHERE table_name ='characteristics'"
+            console.log(SQL1);
+            client.query(SQL1,
+                (err, characteristics)=> {
                     callback(null, {
                         statusCode: '200',
                         "headers": {
                             "Access-Control-Allow-Origin": "*",
                             'Access-Control-Allow-Methods': 'POST'
                         },
-                        body: 'Ошибка соединения с базой данных '
+                        body: characteristics.rows
                     });
-                });
-            } else {
-                console.log('Connection established with pg db server');
-                console.log("SELECT name FROM users WHERE name=\'"+name+"\' or mail= \'"+mail+"\';");
-                client.query("SELECT name FROM users WHERE name=\'"+name+"\' or mail= \'"+mail+"\';",
-                    (err, users)=>{
-                        console.log(users);
-                        console.log(users.rows);
-                        console.log(users.rows.length);
-                        if(!(users.rows.length===0))
-                        {
-                            client.end(function (err) {
-                                callback(null, {
-                                    statusCode: '200',
-                                    "headers": {
-                                        "Access-Control-Allow-Origin": "*",
-                                        'Access-Control-Allow-Methods': 'POST'
-                                    },
-                                    body: "Пользователь с таким именем или почтой уже существует"
-                                });
-                            });
-                        }
-                        else
-                        {
-                            console.log("next query");
-                            client.query("INSERT INTO users(name,mail,password) VALUES(\'" + name + "\',\'" + mail + "\',\'" + password + "\');",
-                                function () {
 
-                                    client.end(function (err) {
-
-
-                                        callback(null, {
-                                            statusCode: '200',
-                                            "headers": {
-                                                "Access-Control-Allow-Origin": "*",
-                                                'Access-Control-Allow-Methods': 'POST'
-                                            },
-                                            body: "success"
-                                        });
-                                    });
-                                });
-                        }
-                    })
-
-
+                })
             }
+
+        }
+
+
+
     });
         */
 
